0862ad0bc1557044b4f8281f38fcfb61268e04aa
chooyan-eng <chooyan.engineer@gmail.com>
2018-08-07 11:57:49 +0900

    [modify] apply IBInspectable

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -13,6 +13,16 @@ class RatingControl: UIStackView {
     //MARK: Properties
     private var ratingButtons = [UIButton]()
     var rating = 0
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+        didSet {
+            setupButtons()
+        }
+    }
+    @IBInspectable var starCount: Int = 5 {
+        didSet {
+            setupButtons()
+        }
+    }
     
     /*
     // Only override draw() if you perform custom drawing.
@@ -41,14 +51,21 @@ class RatingControl: UIStackView {
     //MARK: Private Methods
     private func setupButtons() {
         
-        for _ in 0..<5 {
+        // clear any existing buttons
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        
+        for _ in 0..<starCount {
             let button = UIButton()
             button.backgroundColor = UIColor.red
             
             // Add constraints
             button.translatesAutoresizingMaskIntoConstraints = false
-            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
             
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
             
@@ -59,4 +76,10 @@ class RatingControl: UIStackView {
             ratingButtons.append(button)
         }
     }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupButtons()
+    }
+
 }
