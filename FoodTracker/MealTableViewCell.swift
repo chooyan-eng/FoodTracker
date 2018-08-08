8808c23484862639dbcf592970b9026a3af60937
chooyan-eng <chooyan.engineer@gmail.com>
2018-08-08 16:10:00 +0900

    [add] TableViewController class

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Tsuyoshi Chujo on 2018/08/08.
+//  Copyright © 2018年 Tsuyoshi Chujo. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
